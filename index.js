55dfdf37b2a4915bd5634f313d6c33e818bca2ef
michael <michaelglenn88@yahoo.com>
2022-03-29 20:58:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+// function findMatching(arr, str) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     //log(el)
+//     if (arr[i].toUpperCase() == str.toUpperCase()) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+//or
+function findMatching(arr, str) {
+  return arr.filter((arr) => arr.toUpperCase() === str.toUpperCase());
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter((arr) => arr.startsWith(str));
+}
+
+function matchName(arr, str) {
+  return arr.filter((arr) => arr.name === str);
+}
